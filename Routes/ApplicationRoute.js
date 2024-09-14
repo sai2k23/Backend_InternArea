a9aa11e008e2325d4c88499895f97c6f8729ebb5
K.SAIKRISHNA <saikrishnakolleti@gmail.com>
2024-09-14 11:20:25 +0530

    first commit

---
+++ Routes/ApplicationRoute.js
@@ -0,0 +1,79 @@
+const express =require("express")
+const router =express.Router();
+const application=require("../Model/Application");
+
+router.post("/",async (req,res)=>{
+    const applicationData=new application({
+        coverLetter:req.body.coverLetter,
+        user:req.body.user,
+        company:req.body.company,
+        category:req.body.category,
+        body:req.body.body,
+        ApplicationId:req.body.ApplicationId
+    })
+    await applicationData.save().then((data)=>{
+        res.send(data)
+    }).catch((error)=>{
+        console.log(error,"not able to post the data")
+        res.status(500).json({ error: "Internal server error" });
+    })
+})
+router.get("/", async (req,res)=>{
+    try {
+        const data=await application.find();
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({error:"Internal server error "});
+    }
+})
+router.get("/:id", async (req,res)=>{
+    const {id}=req.params;
+    try {
+        const data=await application.findById(id);
+        if (!data) {
+
+             res.status(404).json({error:"Application is not found "})
+        }
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({error:"Internal server error "})
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const { action } = req.body;
+
+    let status;
+
+    if (action === "accepted") {
+        status = "accepted";
+    } else if (action === "rejected") {
+        status = "rejected";
+    } else {
+        res.status(400).json({ error: "Invalid action" });
+        return; 
+    }
+
+    try {
+        const updateApplication = await application.findByIdAndUpdate(
+            id,
+            { $set: { status } },
+            { new: true }
+        );
+
+        if (!updateApplication) {
+            return res.status(404).json({ error: "Not able to update the application" });
+          
+        }
+
+        res.status(200).json({ success: true, data: updateApplication });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+module.exports=router
